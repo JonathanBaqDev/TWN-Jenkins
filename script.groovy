c8784019358fcc24912ff42adca7b5f7a7a43a5c
JonathanB <jbaquirin@gmail.com>
2026-08-27 07:38:16 +1000

    Configure pipeline push to Dockerhub repo

--- script.groovy
+++ script.groovy
@@ -1,9 +1,19 @@
 def buildJar() {
     echo 'building the application...'
+    sh 'mvn package'
 }
 
 def buildImage() {
     echo "building the docker image..."
+    withCredentials([
+        usernamePassword(
+            credentialsId: 'dockerhub-repo', 
+            usernameVariable: 'USERNAME', 
+            passwordVariable: 'PASSWORD')]) {
+                sh 'docker build -t jbaquirindev/twn-demo:jma-2.0 .'
+                sh 'echo $PASSWORD | docker login -u $USERNAME --password-stdin'
+                sh 'docker push jbaquirindev/twn-demo:jma-2.0'
+    }
 }
 
 def deployApp() {
